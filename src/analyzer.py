9a5f586f59709b07c100f39a2c127b869cd73e39
notswiss <nonsvizzero@gmail.com>
2020-08-23 13:23:39 +0200

    Refactoring

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,45 +1,35 @@
-import datetime
-import itertools
 import json
 
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-from colour import Color
 
-from models import Ruler, Province
+from models import Campaign, START_DATE
 from src import ASSETS_DIR
-from util import get_date, yield_info, calculate_months_diff, timing
+from util import get_date, timing
 
 
 class Analyzer:
-    start_date = datetime.datetime(year=1444, month=11, day=11)
-    # https://eu4.paradoxwikis.com/Template:Revolutionary_flag
-    rev_colors = [(255, 255, 255), (20, 20, 20), (131, 0, 146), (121, 0, 41), (103, 0, 5), (185, 0, 0),
-                  (195, 83, 0), (66, 40, 20), (245, 193, 0), (0, 59, 9), (0, 124, 52), (0, 194, 111),
-                  (0, 175, 194), (53, 0, 131), (110, 194, 243), (26, 50, 134), (211, 0, 27)]
-
-    def __init__(self, gameinfo):
-        self.gameinfo = gameinfo
-        self.player = gameinfo["meta"]["player"]
-        self.current_date = get_date(gameinfo["meta"]["date"])
-        self.countries = gameinfo["gamestate"]["countries"]
-        self.provinces = [Province(id=int(i[1:]), **p) for i, p in gameinfo["gamestate"]["provinces"].items()]
+    def __init__(self):
         with open(f"{ASSETS_DIR}/province_coordinates.json") as f:
             self.province_coordinates = json.load(f)
         self.map_img = Image.open(f"{ASSETS_DIR}/provinces_bordered.png")
         self.map_img_pixels = np.array(self.map_img)
+        self.font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 15, encoding='unic')
+        # todo load config here for drawing, etc
+        self.campaign = None
+
+    def analyze(self, campaign):
+        self.campaign = campaign
+        self.draw_conquest_heat_map()
 
     @timing
     def draw_conquest_heat_map(self, country=None, crop_margin=50, resize_ratio=1.0, start_date=None, end_date=None):
-        country = self.player if country is None else country
-        start_date = get_date(start_date) if start_date else self.start_date
-        end_date = (get_date(end_date) if end_date else self.current_date) + datetime.timedelta(days=1)
-        provinces = self.get_conquest_history(country=country, end_date=end_date)
-        months_diff = [max(calculate_months_diff(p.last_conquest, start_date), 0) for p in provinces]
-        colors = [self.rev_colors[i] for i in self.countries[country]["colors"]["revolutionary_colors"].values()]
-        spectrum = self.calculate_spectrum(colors=colors, parts=end_date.year - start_date.year + 1)
-        id_to_color = {str(p.id): spectrum[max(p.last_conquest.year - start_date.year, 0)]
-                       for p, months in zip(provinces, months_diff)}
+        country = self.campaign.get_country(country)
+        start_date = get_date(start_date) if start_date else START_DATE
+        end_date = get_date(end_date) if end_date else self.campaign.current_date  # todo raise exception wrong date
+        provinces = [p for p in country.owned_provinces if p.last_conquest <= end_date]
+        spectrum = country.calculate_color_spectrum(n=end_date.year - start_date.year + 1)
+        id_to_color = {str(p.id): spectrum[max(p.last_conquest.year - start_date.year, 0)] for p in provinces}
         width, height = self.map_img.size
         pixels = self.map_img_pixels.copy()
         e, n, w, s = 0, height, width, 0
@@ -55,83 +45,31 @@ class Analyzer:
         out = Image.fromarray(pixels)
         if out.size[0] > 50 and crop_margin >= 0:
             out = out.crop((w, n, e, s))
-            self.draw_legend(out, colors, start_date, end_date)
+            w, h = out.size
+            # draw legend
+            draw = ImageDraw.Draw(out)
+            draw.fontmode = '1'
+            margin, height = 10, 30
+            box_width = (w - 2 * margin)
+            # fixme test functioning for conquest after a single year on 3-color countries
+            if start_date.year != end_date.year:
+                spectrum = country.calculate_color_spectrum(n=box_width)
+                for i, c in enumerate(spectrum):
+                    draw.rectangle([(margin + i, h - margin - height), (margin + i + 1, h - margin)], fill=c)
+                draw.rectangle([(margin, h - margin - height), (w - margin, h - margin)], outline="black", width=1)
+            else:
+                draw.rectangle([(margin, h - margin - height), (w - margin, h - margin)],
+                               outline="black", width=1, fill=country.colors[0])
+            font_margin = 8
+            draw.text((margin + font_margin, h - margin - height + font_margin),
+                      str(start_date.year), font=self.font, fill='white')
+            draw.text((w - margin - 42, h - margin - height + font_margin),
+                      str(end_date.year), font=self.font, fill='white')
         out = out.resize(np.array(out.size) * resize_ratio, Image.BILINEAR)
         out.save(f"{ASSETS_DIR}/heatmap.png")
 
-    def draw_legend(self, im, colors, start_date, end_date):
-        font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf', 15, encoding='unic')
-        draw = ImageDraw.Draw(im)
-        draw.fontmode = '1'
-        margin, height = 10, 30
-        w, h = im.size
-        box_width = (w - 2 * margin)
-        if start_date.year != end_date.year:
-            spectrum = self.calculate_spectrum(colors, parts=box_width)
-            for i, c in enumerate(spectrum):
-                draw.rectangle([(margin + i, h - margin - height), (margin + i + 1, h - margin)], fill=c)
-            draw.rectangle([(margin, h - margin - height), (w - margin, h - margin)], outline="black", width=1)
-        else:
-            draw.rectangle([(margin, h - margin - height), (w - margin, h - margin)],
-                           outline="black", width=1, fill=colors[0])
-        font_margin = 8
-        draw.text((margin + font_margin, h - margin - height + font_margin),
-                  str(start_date.year), font=font, fill='white')
-        draw.text((w - margin - 42, h - margin - height + font_margin),
-                  str(end_date.year), font=font, fill='white')
-
-    @staticmethod
-    def calculate_spectrum(colors, parts):
-        """Calculate the color spectrum. Some countries (like sweden) have the first color equal to the last, in that
-        case the spectrum is just a range between the two colors to avoid confusion."""
-        c1, c2, c3 = (Color(rgb=map(lambda x: x / 255, color)) for color in colors)
-        if c1 == c3:
-            it = c1.range_to(c2, parts)
-        else:
-            steps = parts // 2 + 1
-            it = itertools.chain(c1.range_to(c2, steps), c2.range_to(c3, steps))
-        spectrum = [tuple(map(lambda x: round(x * 255), c.rgb)) for c in it]
-        return spectrum[:-(len(spectrum) - parts + 1)] + [spectrum[-1]]
-
-    def get_conquest_history(self, country=None, end_date=None):
-        country = self.player if country is None else country
-        end_date = end_date if end_date else self.current_date
-        conquests = list(sorted((p for p in self.provinces if getattr(p, "owner", None) == country and
-                                 p.last_conquest <= end_date),
-                                key=lambda p: p.last_conquest))
-        return conquests
-
-    def get_ruler_history(self, country=None):
-        country = self.player if country is None else country
-        rulers = []
-        results = {}
-        total_months = 0
-        last_crowning = last_ruler = None
-        for date, history in yield_info(self.countries[country]["history"], f=lambda x: x[0] == '1'):
-            if any(x in history for x in ('monarch', 'monarch_heir')):
-                new_crowing = get_date(date)
-                if new_crowing > self.start_date:
-                    months_diff = calculate_months_diff(new_crowing, last_crowning)
-                    last_ruler.months = months_diff
-                    total_months += months_diff
-                    rulers.append(last_ruler)
-                last_crowning, last_ruler = new_crowing, Ruler(**history.popitem()[1])
-        months_diff = calculate_months_diff(self.current_date, last_crowning)
-        last_ruler.months = months_diff
-        total_months += months_diff
-        rulers.append(last_ruler)
-        # rulers stats
-        results["avg_months_active"] = np.average([r.months for r in rulers if not r.is_regency_council])
-        results["avg_mana"] = sum([r.mana_generated() for r in rulers]) / total_months
-        return results
-
-    @classmethod
-    def from_file(cls, filename):
-        with open(filename) as f:
-            d = json.load(f)
-            return cls(gameinfo=d)
-
 
 if __name__ == '__main__':
-    analyzer = Analyzer.from_file(f"{ASSETS_DIR}/emperor.json")
-    analyzer.draw_conquest_heat_map(resize_ratio=2)
+    analyzer = Analyzer()
+    campaign = Campaign.from_file(f"{ASSETS_DIR}/emperor.json")
+    analyzer.analyze(campaign)
--- src/models.py
+++ src/models.py
@@ -1,8 +1,83 @@
+import itertools
+import json
 from datetime import datetime
 
 import numpy as np
+from colour import Color
 
-from util import yield_info, get_date
+from util import yield_info, get_date, calculate_months_diff
+
+START_DATE = datetime(year=1444, month=11, day=11)
+
+
+class Campaign:
+    def __init__(self, gameinfo):
+        self.gameinfo = gameinfo
+        self.player = gameinfo["meta"]["player"]
+        self.current_date = get_date(gameinfo["meta"]["date"])
+        self.provinces = [Province(id=int(i[1:]), **p) for i, p in gameinfo["gamestate"]["provinces"].items()]
+        self.countries = {tag: Country(self, tag=tag, **c) for tag, c in gameinfo["gamestate"]["countries"].items()}
+
+    def get_country(self, country=None):
+        return self.countries[country if country else self.player]
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename) as f:
+            d = json.load(f)
+            return cls(gameinfo=d)
+
+
+class Country:
+    # https://eu4.paradoxwikis.com/Template:Revolutionary_flag
+    REV_COLORS = [(255, 255, 255), (20, 20, 20), (131, 0, 146), (121, 0, 41), (103, 0, 5), (185, 0, 0),
+                  (195, 83, 0), (66, 40, 20), (245, 193, 0), (0, 59, 9), (0, 124, 52), (0, 194, 111),
+                  (0, 175, 194), (53, 0, 131), (110, 194, 243), (26, 50, 134), (211, 0, 27)]
+
+    def __init__(self, campaign, **kwargs):
+        self.__dict__.update(**kwargs)
+        for k in ('owned_provinces', 'controlled_provinces', 'core_provinces'):
+            setattr(self, k, list(sorted((campaign.provinces[i - 1] for i in kwargs[k].values()),
+                                         key=lambda p: p.last_conquest)))
+        # todo find meanings of 'adm_spent_indexed' with the "powerspend" command in-game
+        for k in ('capital', 'trade_port'):
+            setattr(self, k, campaign.provinces[kwargs[k]])
+        self.rulers = []
+        self.avg_ruler_stats = None
+        self.avg_ruler_life = None
+        self.get_ruler_history(campaign.current_date)
+        self.colors = [self.REV_COLORS[i] for i in kwargs["colors"]["revolutionary_colors"].values()]
+
+    def get_ruler_history(self, current_date):
+        total_months = calculate_months_diff(current_date, START_DATE)
+        last_crowning = last_ruler = None
+        for date, history in yield_info(self.history, f=lambda x: x[0] == '1'):
+            if any(x in history for x in ('monarch', 'monarch_heir')):
+                new_crowing = get_date(date)
+                if new_crowing > START_DATE:
+                    self.add_ruler(last_ruler, new_crowing, last_crowning)
+                last_crowning, last_ruler = new_crowing, Ruler(**history.popitem()[1])
+        self.add_ruler(last_ruler, current_date, last_crowning)
+        # rulers stats
+        self.avg_ruler_life = np.average([r.months for r in self.rulers if not r.is_regency_council])
+        self.avg_ruler_stats = sum([r.mana_generated for r in self.rulers]) / total_months
+
+    def add_ruler(self, ruler, d1, d2):
+        months_diff = calculate_months_diff(d1, d2)
+        ruler.set_lifespan(months_diff)
+        self.rulers.append(ruler)
+
+    def calculate_color_spectrum(self, n):
+        """Calculate the color spectrum. Some countries (like sweden) have the first color equal to the last, in that
+        case the spectrum is just a range between the two colors to avoid confusion."""
+        c1, c2, c3 = (Color(rgb=map(lambda x: x / 255, color)) for color in self.colors)
+        if c1 == c3:
+            it = c1.range_to(c2, n)
+        else:
+            steps = n // 2 + 1
+            it = itertools.chain(c1.range_to(c2, steps), c2.range_to(c3, steps))
+        spectrum = [tuple(map(lambda x: round(x * 255), c.rgb)) for c in it]
+        return spectrum[:-(len(spectrum) - n + 1)] + [spectrum[-1]]
 
 
 class Province:
@@ -31,9 +106,11 @@ class Ruler:
         self.value = np.array([kwargs[x] for x in self.stats])
         self.is_regency_council = kwargs['name'] == "(Regency Council)"
         self.months = None
+        self.mana_generated = None
 
     def __str__(self):
         return f"value={self.value}, months={self.months}"
 
-    def mana_generated(self):
-        return self.value * self.months
+    def set_lifespan(self, months):
+        self.months = months
+        self.mana_generated = self.value * self.months
